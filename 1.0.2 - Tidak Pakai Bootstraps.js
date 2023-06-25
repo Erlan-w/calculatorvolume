1994531d7b8000db8eeed0084be9c6cb9bf34b85
Erlan Wahyu <80790147+Erlan-w@users.noreply.github.com>
2023-06-26 00:41:45 +0700

    add files via upload

---
+++ 1.0.2 - Tidak Pakai Bootstraps.js
@@ -0,0 +1,96 @@
+let form = document.getElementById( 'form' );
+let tmbField = document.getElementById( 'tmbField' );
+
+// tambah field
+let k = 2;
+tmbField.onclick = ( e ) => {
+	let fieldset = document.createElement( 'fieldset' );
+	let isPal = document.getElementById( 'isPal' );
+
+
+	let isi = `
+		<legend>Tempat ${k}</legend>
+					<p>
+						<label for="isPal">Isi/ Palet :</label>
+						<input type="number" name="isPal" id="isPal" value="${isPal.value}" />
+					</p>
+
+					<p>
+						<label for="jumPal" class="form-label">Jumlah Palet : </label>
+						<input type="number" name="jumPal" id="jumPal" class="form-control" onkeyup="hitung()" />
+					</p>
+					<p><label for="hasil">Hasil: </label>
+						<input type="number" name="hasil" id="hasil" disabled />
+					</p>
+	`;
+
+	fieldset.id = 'tmp_' + k;
+	fieldset.innerHTML = isi;
+
+	form.appendChild( fieldset );
+	bindTaskEvents( fieldset );
+	k++;
+	e.preventDefault();
+}
+
+let hasVal = [];
+
+var bindTaskEvents = function ( taskListItem ) {
+	// console.log( "Bind list item events" );
+
+	//select taskListItem's children
+	// let delBtn = taskListItem.querySelector( "button.btn" );
+	let isPal = taskListItem.querySelector( 'input#isPal' );
+	let jumPal = taskListItem.querySelector( 'input#jumPal' );
+	let hasil = taskListItem.querySelector( 'input#hasil' );
+
+	console.log( taskListItem );
+
+
+
+	//bind deleteTask to delete button
+	// fungsi hitung pertempat
+	jumPal.onkeyup = () => {
+		let valIsPal = isPal.value;
+		let valJumPal = jumPal.value;
+
+		hasil.value = valIsPal * valJumPal;
+		hasVal.push( hasil.value );
+	}
+
+}
+
+
+//cycle over incompleteTasksHolder ul list items
+for ( var i = 0; i < form.children.length; i++ ) {
+
+	//bind events to list item's children (taskCompleted)
+	bindTaskEvents( form.children[i] );
+
+	// console.log( form.children[i] );
+}
+
+
+// total
+
+// let totHasil = () => {
+let totBtn = document.querySelector( 'button#totBtn' );
+let totHas = document.getElementById( 'totHas' );
+
+let hasil = 0;
+
+totBtn.onclick = () => {
+
+	// cycle for total hasil
+	for ( let j = 0; j < hasVal.length; j++ ) {
+		hasil += parseInt( hasVal[j] );
+	}
+
+	console.log( hasil );
+	// console.log( taskListItem );
+	totHas.innerText = hasil;
+}
+	// }
+
+
+
