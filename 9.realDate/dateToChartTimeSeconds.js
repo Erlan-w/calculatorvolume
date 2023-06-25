89253b9ece24eec2b355d6340efb672540f274b9
Erlan Wahyu <80790147+Erlan-w@users.noreply.github.com>
2023-06-26 00:41:08 +0700

    Delete 9.realDate Directory - salah upload

--- 9.realDate/dateToChartTimeSeconds.js
+++
@@ -1,3 +0,0 @@
-export let dateToChartTimeSeconds = ( date ) => {
-	return Date.UTC( date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds(), 0 ) / 1000;
-};
